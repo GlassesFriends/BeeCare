2c847ee7d3f4b8ca35b00423188635f5e63aa51c
iscRamirezAbril <isc.ramirez_abril@outlook.com>
2022-11-20 13:57:59 -0800

    Chatbot implementado.

---
+++ static/js/chatbot.js
@@ -0,0 +1,8 @@
+(function(d, m){
+    var kommunicateSettings = 
+        {"appId":"7f3f4d355a819e5b93f7a49aec24617","popupWidget":true,"automaticChatOpenOnNavigation":true};
+    var s = document.createElement("script"); s.type = "text/javascript"; s.async = true;
+    s.src = "https://widget.kommunicate.io/v2/kommunicate.app";
+    var h = document.getElementsByTagName("head")[0]; h.appendChild(s);
+    window.kommunicate = m; m._globals = kommunicateSettings;
+})(document, window.kommunicate || {});
