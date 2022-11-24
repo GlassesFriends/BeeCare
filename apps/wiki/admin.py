f7ea4b2e5e6952ad2cbaec8cb987c19cafd0cb2d
iscRamirezAbril <isc.ramirez_abril@outlook.com>
2022-11-23 19:02:06 -0800

    Archivo admin.py para la app wiki.

--- apps/wiki/admin.py
+++ apps/wiki/admin.py
@@ -1,3 +1,23 @@
+# |=============================================================|
+# |===============|         BIBLIOTECAS         |===============|
+# |=============================================================|
+
+# |=========================================|
+# |=====|       BIBLIOTECAS BASE      |=====|
+# |=========================================|
 from django.contrib import admin
 
+from .models import Category, BeePost, Comment
+
 # Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ["id", "categoryTitle"]
+    
+@admin.register(BeePost)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ["id", "bPostTitle", "bPostSlug", "bPostCreated_at"]
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ["id", "commentName", "commentEmail", "commentCreated_at"]
