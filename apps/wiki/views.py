b7b1cee08aec77c6804a3fb99e4b34a32b58e082
iscRamirezAbril <isc.ramirez_abril@outlook.com>
2022-11-23 20:54:11 -0800

    Sección wiki finalizada.

--- apps/wiki/views.py
+++ apps/wiki/views.py
@@ -6,33 +6,34 @@
 # |=====|       BIBLIOTECAS EXTRA     |=====|
 # |=========================================|
 from django.shortcuts import get_object_or_404, render, redirect
+from apps.member.decorators import user_auth
 
 # |=========================================|
 # |=====|       BIBLIOTECAS EXTRA     |=====|
 # |=========================================|
-from .models import Category, BeePost, Comment
+from .models import category, beePost, comment
 
 # |=| Método para la página con la lista de artículos publicados |=|
 def wikihome(request):
-    posts = BeePost.objects.all()
+    posts = beePost.objects.all()
 
     return render(request, 'wiki/wikihome.html', {'posts': posts})
 
 # |=| Método para la asignación de página para cada artículo |=|
 def article_detail(request, bPostSlug):
-    post = get_object_or_404(BeePost, bPostSlug=bPostSlug)
+    post = get_object_or_404(beePost, bPostSlug=bPostSlug)
 
     if request.method == 'POST':
-        name = request.POST.get('name', '')
-        comment = request.POST.get('comment', '')
+        memberObj = request.user.member
+        commentText = request.POST.get('membComment', '')
 
-        if name and comment:
-            Comment.objects.create(
-                post=post,
-                name=name,
-                comment=comment
+        if commentText:
+            comment.objects.create(
+                commentBeepost=post,
+                commentMember=memberObj,
+                commentText=commentText
             )
 
-            return redirect('post_detail', bPostSlug=post.bPostSlug)
+            return redirect('article_detail', bPostSlug=post.bPostSlug)
 
     return render(request, 'wiki/detail.html', {'post': post})
