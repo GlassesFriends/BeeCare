ef9676342ff55188d17f5f82a97aacfecc63e080
iscRamirezAbril <isc.ramirez_abril@outlook.com>
2022-11-23 18:50:06 -0800

    Creación de modelos para la app de wiki.

--- apps/wiki/models.py
+++ apps/wiki/models.py
@@ -1,3 +1,54 @@
+# |=============================================================|
+# |===============|         BIBLIOTECAS         |===============|
+# |=============================================================|
+
+# |=========================================|
+# |=====|       BIBLIOTECAS BASE      |=====|
+# |=========================================|
 from django.db import models
 
+# |==============================================|
+# |=====|       BIBLIOTECAS ADICIONALES     |====|
+# |==============================================|
+from statistics import mode
+from django.db import models
+from pathlib import Path
+
 # Create your models here.
+
+# |=========================================|
+# |========|     MODELO CATEGORY    |=======|
+# |=========================================|
+class Category(models.Model):
+    categoryTitle = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.categoryTitle
+
+# |=========================================|
+# |========|     MODELO BEEPOST     |=======|
+# |=========================================|
+class BeePost(models.Model):
+    bPostCategory = models.ForeignKey(Category, related_name='posts', on_delete=models.CASCADE)
+    bPostCoverImage = models.ImageField(upload_to='BeePost/',blank=True)
+    bPostTitle = models.CharField(max_length=255)
+    bPostSlug = models.SlugField(max_length=255)
+    bPostIntro = models.TextField()
+    bPostBody = models.TextField()
+    bPostCreated_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ('-created_at', )
+
+    def __str__(self):
+        return self.bPostTitle
+
+# |=========================================|
+# |========|     MODELO COMMENT     |=======|
+# |=========================================|
+class Comment(models.Model):
+    commentBeepost = models.ForeignKey(BeePost, related_name='comments', on_delete=models.CASCADE)
+    commentName = models.CharField(max_length=255)
+    commentEmail = models.EmailField(max_length=255)
+    commentText = models.TextField()
+    commentCreated_at = models.DateTimeField(auto_now_add=True)
