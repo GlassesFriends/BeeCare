c0a64fc4af91e18999daf3fdbd4ae3cde8bbc1a3
corona9955 <daniel.corona193@tectijuana.edu.mx>
2023-05-19 16:34:53 -0700

    Corrección de acceso a login y registro.

--- apps/member/views.py
+++ apps/member/views.py
@@ -124,99 +124,101 @@ def memberSignOut(request):
 # @user_auth
 # @allowed_users(allowed_roles=['admin'])
 def memberSignUp(request):
-    
-    # |=| Arreglo datos en caso de error  |=| 
-    data_error = ["","","","",""]
-    print(data_error[0])
-    # |=| Inicializamos una instancia del |=|
-    # |=| formulario para registrar un    |=|
-    # |=| usuario.                        |=|
-    form = CreateUserForm()
-    
-    # |=| Validamos el método de nuestro  |=|
-    # |=| método POST.                    |=|
-    if request.method == 'POST':
-        userForm = CreateUserForm(request.POST,request.FILES)
-        # |=| Validamos el correo  si es      |=|
-        # |=| que este existe.                |=|
-        emailtest = request.POST['username']
-        # |=| si que este existe.             |=|
-        # |=| Evitamos que se borren datos    |=|
-        # |=| importantes del usuario         |=|
-        # |=| imagenes y contraseña deben de  |=|
-        # |=| reingresarse por seguridad de   |=|
-        # |=| los equipos.                    |=|
-        # |=| Retorna una advertencia.        |=|
-        if member.objects.filter(membEmail=emailtest).exists():         
-            data_error.insert(2,request.POST['first_name'])
-            data_error.insert(3,request.POST['last_name'])
-            data_error.insert(4,request.POST['membDateBirth'])
-            data_error.insert(5,request.POST['username'])
-            data_error.insert(6,request.POST['membPhone'])
-            messages.warning(request,"El correo que desea registrar ya existe.") 
-            context = {
-                'error': data_error,        
-            }
-            return render(request, 'member/signup.html',context )          
-        # |=| si no existe este.             |=|
-        # |=| Continua con el formulario.    |=|
-        else:
-            # |=| Validamos el método de nuestro  |=|
-            # |=| formulario.                     |=|    
-            if userForm.is_valid():
-                # |=| Capturamos los datos del|=|
-                # |=| formulario.             |=|
-                user = userForm.save()
-                username = userForm.cleaned_data.get('username')
-                email = userForm.cleaned_data.get('username')
-                first_name = userForm.cleaned_data.get('first_name')    
-                last_name = userForm.cleaned_data.get('last_name')
-                print(request.POST.get('membDateBirth'))
-                print(request.POST.get('membPhone'))
-                print(request.FILES.get('membProfilePicture'))
-                
-                # |=| Asignamos el grupo      |=|
-                # |=| 'member' al nuevo user. |=|
-                group, created  = Group.objects.get_or_create(name ='member')
-                user.groups.add(group)
-                
-                # |=| Creamos nuestro objeto  |=|
-                # |=| de nuestra tabla de     |=|
-                # |=| relación OneToOne con la|=|
-                # |=| tabla de users de       |=|
-                # |=| django.                 |=|
-                member.objects.create(
-                    membFirstName =         user.first_name,
-                    membLastName =          user.last_name,
-                    
-                    # |=| Se obtiene la data  |=|
-                    # |=| del formulario por  |=|
-                    # |=| medio del método    |=|
-                    # |=| request.            |=|
-                    membDateBirth =         request.POST.get('membDateBirth'),
-                    membPhone =             request.POST.get('membPhone'),
-                    # |=| Usar FILES en lugar |=|
-                    # |=| de POST para imagen |=|
-                    membProfilePicture =    request.FILES.get('membProfilePicture'),
-                    
-                    membEmail =             user.username,
-                    membUser =              user,
-                )
-                
-                # |=| Cuando finaliza el      |=|
-                # |=| registro, se redirije al|=|
-                # |=| usuario a nuestra pag.  |=|
-                # |=| de inicio de sesión.    |=|
-                text = 'Usuario registrado exitosamente, regresa e inicia sesión.'
-                messages.success(request, text)
-                return redirect(reverse('signin'))
+    if request.user.is_authenticated:
+        return redirect(reverse('home'))
+    else:
+        # |=| Arreglo datos en caso de error  |=| 
+        data_error = ["","","","",""]
+        print(data_error[0])
+        # |=| Inicializamos una instancia del |=|
+        # |=| formulario para registrar un    |=|
+        # |=| usuario.                        |=|
+        form = CreateUserForm()
+        
+        # |=| Validamos el método de nuestro  |=|
+        # |=| método POST.                    |=|
+        if request.method == 'POST':
+            userForm = CreateUserForm(request.POST,request.FILES)
+            # |=| Validamos el correo  si es      |=|
+            # |=| que este existe.                |=|
+            emailtest = request.POST['username']
+            # |=| si que este existe.             |=|
+            # |=| Evitamos que se borren datos    |=|
+            # |=| importantes del usuario         |=|
+            # |=| imagenes y contraseña deben de  |=|
+            # |=| reingresarse por seguridad de   |=|
+            # |=| los equipos.                    |=|
+            # |=| Retorna una advertencia.        |=|
+            if member.objects.filter(membEmail=emailtest).exists():         
+                data_error.insert(2,request.POST['first_name'])
+                data_error.insert(3,request.POST['last_name'])
+                data_error.insert(4,request.POST['membDateBirth'])
+                data_error.insert(5,request.POST['username'])
+                data_error.insert(6,request.POST['membPhone'])
+                messages.warning(request,"El correo que desea registrar ya existe.") 
+                context = {
+                    'error': data_error,        
+                }
+                return render(request, 'member/signup.html',context )          
+            # |=| si no existe este.             |=|
+            # |=| Continua con el formulario.    |=|
             else:
-                messages.error(request,"Error al guardar los datos.")              
-    context = {
-        'signUp': 'active',
-        'error': data_error    
-        }
-    return render(request, 'member/signup.html', context)
+                # |=| Validamos el método de nuestro  |=|
+                # |=| formulario.                     |=|    
+                if userForm.is_valid():
+                    # |=| Capturamos los datos del|=|
+                    # |=| formulario.             |=|
+                    user = userForm.save()
+                    username = userForm.cleaned_data.get('username')
+                    email = userForm.cleaned_data.get('username')
+                    first_name = userForm.cleaned_data.get('first_name')    
+                    last_name = userForm.cleaned_data.get('last_name')
+                    print(request.POST.get('membDateBirth'))
+                    print(request.POST.get('membPhone'))
+                    print(request.FILES.get('membProfilePicture'))
+                    
+                    # |=| Asignamos el grupo      |=|
+                    # |=| 'member' al nuevo user. |=|
+                    group, created  = Group.objects.get_or_create(name ='member')
+                    user.groups.add(group)
+                    
+                    # |=| Creamos nuestro objeto  |=|
+                    # |=| de nuestra tabla de     |=|
+                    # |=| relación OneToOne con la|=|
+                    # |=| tabla de users de       |=|
+                    # |=| django.                 |=|
+                    member.objects.create(
+                        membFirstName =         user.first_name,
+                        membLastName =          user.last_name,
+                        
+                        # |=| Se obtiene la data  |=|
+                        # |=| del formulario por  |=|
+                        # |=| medio del método    |=|
+                        # |=| request.            |=|
+                        membDateBirth =         request.POST.get('membDateBirth'),
+                        membPhone =             request.POST.get('membPhone'),
+                        # |=| Usar FILES en lugar |=|
+                        # |=| de POST para imagen |=|
+                        membProfilePicture =    request.FILES.get('membProfilePicture'),
+                        
+                        membEmail =             user.username,
+                        membUser =              user,
+                    )
+                    
+                    # |=| Cuando finaliza el      |=|
+                    # |=| registro, se redirije al|=|
+                    # |=| usuario a nuestra pag.  |=|
+                    # |=| de inicio de sesión.    |=|
+                    text = 'Usuario registrado exitosamente, regresa e inicia sesión.'
+                    messages.success(request, text)
+                    return redirect(reverse('signin'))
+                else:
+                    messages.error(request,"Error al guardar los datos.")              
+        context = {
+            'signUp': 'active',
+            'error': data_error    
+            }
+        return render(request, 'member/signup.html', context)
 
 
 
@@ -240,67 +242,70 @@ def memberSignUp(request):
 # |===antes o después de que la solicitud===|
 # |========llegue al objeto original========|
 
-def memberSignIn(request):   
-    # |=| Validamos el formulario actual  |=|
-    # |=| respecto al métpdp POST.        |=|
-    if request.method == 'POST':
-        # |=| Se obtienen los datos del   |=|
-        # |=| formulario.                 |=|
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-        
-        real_login_access = RealLoginAccess()
-        proxy = Proxy(real_login_access)
-        if (proxy.Access(request,username,password) == True): 
-            # |=| formulario preguntas.            |=|
+def memberSignIn(request):
+    if request.user.is_authenticated:
+        return redirect(reverse('home'))
+    else:
+        # |=| Validamos el formulario actual  |=|
+        # |=| respecto al métpdp POST.        |=|
+        if request.method == 'POST':
+            # |=| Se obtienen los datos del   |=|
+            # |=| formulario.                 |=|
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            
+            real_login_access = RealLoginAccess()
+            proxy = Proxy(real_login_access)
+            if (proxy.Access(request,username,password) == True): 
+                # |=| formulario preguntas.            |=|
 
-            qtyTests = testform.objects.filter(isEnabled=True).count()
-            testToAnswer = None
-            readyToAnswer=False
-            #Valida que exista al menos un test en base de datos.
-            if qtyTests > 0:
-                iAnswerthetest = answerusr.objects.filter(answerusrMember=request.user.member).count()
+                qtyTests = testform.objects.filter(isEnabled=True).count()
+                testToAnswer = None
+                readyToAnswer=False
+                #Valida que exista al menos un test en base de datos.
+                if qtyTests > 0:
+                    iAnswerthetest = answerusr.objects.filter(answerusrMember=request.user.member).count()
 
-                ###############################################################################################
-                # Si el usuario ya ha respondido por lo menos un test se procede a buscar el test que sigue.  #
-                # Después valida el tiempo transcurrido desde la última respuesta.                            #
-                ###############################################################################################
-                if iAnswerthetest > 0:
-                    userAnswers = answerusr.objects.filter(answerusrMember=request.user.member)
-                    lastAnswer = userAnswers.order_by('-answerusrDate').first()
-                    lastTestCompleted = lastAnswer.answerusrQuestion.questionTestform
-                    nextTestId=lastTestCompleted.responseOrder + 1
-                    testToAnswer = testform.objects.filter(responseOrder=nextTestId, isEnabled=True).first()
+                    ###############################################################################################
+                    # Si el usuario ya ha respondido por lo menos un test se procede a buscar el test que sigue.  #
+                    # Después valida el tiempo transcurrido desde la última respuesta.                            #
+                    ###############################################################################################
+                    if iAnswerthetest > 0:
+                        userAnswers = answerusr.objects.filter(answerusrMember=request.user.member)
+                        lastAnswer = userAnswers.order_by('-answerusrDate').first()
+                        lastTestCompleted = lastAnswer.answerusrQuestion.questionTestform
+                        nextTestId=lastTestCompleted.responseOrder + 1
+                        testToAnswer = testform.objects.filter(responseOrder=nextTestId, isEnabled=True).first()
 
-                    lastResTime = datetime.now(pytz.timezone('America/Tijuana')) - lastAnswer.answerusrDate
+                        lastResTime = datetime.now(pytz.timezone('America/Tijuana')) - lastAnswer.answerusrDate
 
-                    environment = config('DEBUG', cast=bool)
-                    if(testToAnswer is not None):
-                        if (lastResTime.total_seconds() >= testToAnswer.responseTime and environment):#En debug se toma por segundos
-                            readyToAnswer = True                       
-                        elif(lastResTime.total_seconds() / 3600 >= testToAnswer.responseTime and not environment):#En prod se toma por horas
-                            readyToAnswer = True
-                        else:
-                            readyToAnswer = False
-                ############################################################################################
-                # Si el usuario no ha respondido ningún test, se asigna el primero.                        #
-                ############################################################################################
+                        environment = config('DEBUG', cast=bool)
+                        if(testToAnswer is not None):
+                            if (lastResTime.total_seconds() >= testToAnswer.responseTime and environment):#En debug se toma por segundos
+                                readyToAnswer = True                       
+                            elif(lastResTime.total_seconds() / 3600 >= testToAnswer.responseTime and not environment):#En prod se toma por horas
+                                readyToAnswer = True
+                            else:
+                                readyToAnswer = False
+                    ############################################################################################
+                    # Si el usuario no ha respondido ningún test, se asigna el primero.                        #
+                    ############################################################################################
+                    else:
+                        testToAnswer = testform.objects.filter(responseOrder=1, isEnabled=True).first()
+                        readyToAnswer = True
                 else:
-                    testToAnswer = testform.objects.filter(responseOrder=1, isEnabled=True).first()
-                    readyToAnswer = True
+                    readyToAnswer = False
+                if readyToAnswer is True and testToAnswer is not None:
+                    url = reverse('formnum', kwargs={'pk':testToAnswer.id})
+                    return redirect(f'{url}')
+                else:
+                    return redirect(reverse('home'))
+                
             else:
-                readyToAnswer = False
-            if readyToAnswer is True and testToAnswer is not None:
-                url = reverse('formnum', kwargs={'pk':testToAnswer.id})
-                return redirect(f'{url}')
-            else:
-                return redirect(reverse('home'))
-            
-        else:
-            messages.info(request, 'Tu usuario o contraseña es incorrecto, si necesitas ayuda comunicate al departamento de sistemas.')
-            
-    context = {}
-    return render(request, 'member/signin.html', context)
+                messages.info(request, 'Tu usuario o contraseña es incorrecto, si necesitas ayuda comunicate al departamento de sistemas.')
+                
+        context = {}
+        return render(request, 'member/signin.html', context)
 
 # |============== Servicio ===============|
 class LoginAccess():
